832e1c7cd584e3fdb80c759f0bf45bd4dac60b7b
Joseph Karim <joekarim87@gmail.com>
2025-04-01 13:34:16 -0400

    Fix: Remove duplicate Starlit Fantasy style code in ArtStyleStep

--- src/components/wizard/ArtStyleStep.jsx
+++ src/components/wizard/ArtStyleStep.jsx
@@ -81,7 +81,7 @@ const dzineStyleImageMap = {
   
   'Style-b484beb8-143e-4776-9a87-355e0456cfa3': cuteExaggerationImg,
   'Style-2ee57e3c-108a-41dd-8b28-b16d0ceb6280': glossyEleganceImg,
-  'Style-541a2afd-904a-4968-bc60-8ad0ede22a86': starlitFantasyImg,
+  'Style-9cde0ca9-78f0-4be5-a6a1-44dd74cfbaa0': starlitFantasyImg,
   'Style-7a23990c-65f7-4300-b2a1-f5a97263e66f': fantasyHeroImg,
   'Style-455da805-d716-4bc8-a960-4ac505aa7875': joyfulClayImg,
   
@@ -98,8 +98,7 @@ const dzineStyleImageMap = {
   'Style-d0fbfa6f-59bb-4578-a567-bde0c82bd833': ceramicLifelikeImg,
   'Style-b3a85eaa-5c3a-4c96-af0f-db5c984a955a': yarnRealismImg,
   'Style-1e39bdee-4d33-4f5b-9bbc-12f8f1505fc6': mysticalSovereigntyImg,
-  'Style-2a7de14d-6712-4115-a6a9-d3c7be55eaf2': softRadianceImg,
-  'Style-9cde0ca9-78f0-4be5-a6a1-44dd74cfbaa0': starlitFantasyImg
+  'Style-2a7de14d-6712-4115-a6a9-d3c7be55eaf2': softRadianceImg
 };
 
 // Updated Art Style Categories with direct API styles and CORRECT names
@@ -200,7 +199,7 @@ export const ART_STYLE_CATEGORIES_STRUCTURE = [
       },
       { 
         id: 'starlit_fantasy',
-        apiCode: 'Style-541a2afd-904a-4968-bc60-8ad0ede22a86',
+        apiCode: 'Style-9cde0ca9-78f0-4be5-a6a1-44dd74cfbaa0',
         title: 'Starlit Fantasy',
         description: 'A dreamy and ethereal style with a magical starlit quality.'
       },
